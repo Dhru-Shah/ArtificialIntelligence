8a6e56379531b57865d382312b2fc06a6c9dc67b
Dhru-Shah <shah.dhrumil@outlook.com>
2022-12-15 04:21:40 -0500

    removed

--- src/main/java/com/chess/Color.java
+++
@@ -1,6 +0,0 @@
-package com.chess;
-
-// defining the two colours of the players both white and black in chess
-public enum Color {
-    WHITE, BLACK
-}
--- src/main/java/com/chess/ai/EvaluationMethod.java
+++
@@ -1,8 +0,0 @@
-package com.chess.ai;
-
-import com.chess.Board;
-
-public interface EvaluationMethod {
-
-    double evaluateBoard(final Board board, final int turn);
-}
